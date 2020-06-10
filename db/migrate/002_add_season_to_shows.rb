7e0d1c5242e285e7164aecccafcf5423130db9d9
Irene Scott <irenejinscott@gmail.com>
2020-06-10 23:40:01 +0000

    Automatically backed up by Learn

--- db/migrate/002_add_season_to_shows.rb
+++ db/migrate/002_add_season_to_shows.rb
@@ -1,5 +1,5 @@
 class AddSeasonToShows < ActiveRecord::Migration[5.2]
   def change
-    add_column :shows, :season, :string
+    add_column :shows, :season, :string #need to define shows table first before adding a column
   end
 end
